3a425588f40ba7e3efbd918f576ff9831625b694
Trần Hạ Lan <halan4444b@gmail.com>
2022-05-12 17:23:17 +0700

    long

---
+++ src/main/java/Model/Customer.java
@@ -0,0 +1,92 @@
+package Model;
+
+public class Customer {
+    private String name;
+    private String birthday;
+    private int Gender;
+    private double grade;
+    private String address;
+    private String profileImage;
+    private int customerType;
+
+    public Customer(String name, String birthday, int gender, double grade, String address, String profileImage, int customerType) {
+        this.name = name;
+        this.birthday = birthday;
+        this.Gender = gender;
+        this.grade = grade;
+        this.address = address;
+        this.profileImage = profileImage;
+        this.customerType = customerType;
+    }
+    public Customer(){}
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public int getGender() {
+        return Gender;
+    }
+
+    public void setGender(int gender) {
+        Gender = gender;
+    }
+
+    public double getGrade() {
+        return grade;
+    }
+
+    public void setGrade(double grade) {
+        this.grade = grade;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getProfileImage() {
+        return profileImage;
+    }
+
+    public void setProfileImage(String profileImage) {
+        this.profileImage = profileImage;
+    }
+
+    public int getCustomerType() {
+        return customerType;
+    }
+
+    public void setCustomerType(int customerType) {
+        this.customerType = customerType;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "name='" + name + '\'' +
+                ", birthday='" + birthday + '\'' +
+                ", Gender=" + Gender +
+                ", grade=" + grade +
+                ", address='" + address + '\'' +
+                ", profileImage='" + profileImage + '\'' +
+                ", customerType=" + customerType +
+                '}';
+    }
+}
+
---
+++ src/main/java/Model/CustomerType.java
@@ -0,0 +1,38 @@
+package Model;
+
+public class CustomerType {
+        private int id;
+        private String customerType;
+
+        public CustomerType(int id, String customerType) {
+            this.id = id;
+            this.customerType = customerType;
+        }
+        public CustomerType(){
+
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public String getCustomerType() {
+            return customerType;
+        }
+
+        public void setCustomerType(String customerType) {
+            this.customerType = customerType;
+        }
+
+    @Override
+    public String toString() {
+        return "CustomerType{" +
+                "id=" + id +
+                ", customerType='" + customerType + '\'' +
+                '}';
+    }
+}
